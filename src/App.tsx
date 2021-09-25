676ea02e38c01c1b72dd5f23a1d79df424ba9d3d
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-09-25 20:25:36 +0500

    added some more adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,17 @@ function App() {
   const startQuiz = async () => {};
   const nextQuestion = async () => {};
   return (
-    <div className=" bg-primary mx-auto p-4 h-screen">
-      <div className="text-center">
-        <h1 className=" text-6xl text-accent_2">Quiz</h1>
+    <div className=" mx-auto p-4 h-screen flex flex-col justify-center items-center">
+      <div className="text-center ">
+        <h1 className=" text-6xl text-accent_2 mt-2">Quiz</h1>
         <button
-          className="start text-accent_2 text-lg px-4 py-1 rounded-lg btn bg-accent_1 "
+          className="start text-accent_2 text-lg px-4 py-1 rounded-lg btn bg-accent_1 my-8"
           onClick={startQuiz}
         >
           Begin Quiz
         </button>
-        <p className="text-4xl text-accent_2">Score : </p>
-        <p className=" text-6xl text-accent_2">Loading :</p>
+        <p className="text-4xl text-accent_2 my-4">Score : </p>
+        <p className=" text-6xl text-accent_2 my-8">Loading :</p>
         <button
           className="next text-accent_2 text-lg px-3 py-1/2 bg-accent_1 rounded-md btn"
           onClick={nextQuestion}
